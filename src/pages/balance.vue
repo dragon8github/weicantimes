835f3af6161accb4e49e5dd9b7d68cd8a69b6719
dragon8github <928532756@qq.com>
2016-12-22 22:20:28 +0800

    为尊严而战

---
+++ src/pages/balance.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'balance',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ src/pages/menu.vue
@@ -0,0 +1,18 @@
+<template>
+</template>
+
+<script>
+export default {
+
+  name: 'menu',
+
+  data () {
+    return {
+
+    };
+  }
+};
+</script>
+
+<style lang="css" scoped>
+</style>
