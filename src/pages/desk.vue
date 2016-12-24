2310e26f913c77dcc2faff3145931f38ca200c3c
dragon8github <928532756@qq.com>
2016-12-25 01:24:47 +0800

    设计和参考成熟APP的界面

--- src/pages/desk.vue
+++ src/pages/desk.vue
@@ -1,108 +1,111 @@
 <template>
 	<div>
-		<ul class="mui-table-view">
-		    <li class="mui-table-view-cell">
-		        <a class="mui-navigate-right">Number of Guests
-        	    <span class="not-function mui-badge mui-badge-danger">Please Choose</span>
-		        </a>
-		    </li>
-		 </ul>  
-		 <div class="floor-ico">
-	 		<span><i class="iconfont icon-user iconfont-eated"></i><span class="floor-ico-text">Eated</span></span>
-	 		<span><i class="iconfont icon-lock iconfont-seize"></i><span class="floor-ico-text">Seize</span></span>
-	 		<span><i class="iconfont icon-yuding iconfont-destine"></i><span class="floor-ico-text">Destine</span></span>
-	 		<span><i class="iconfont icon-tuijian iconfont-recommended"></i><span class="floor-ico-text">Recommended</span></span>
-	 	</div>
-		 <div class="floor floor-1">
-			 	<div class="floor-text">First Floor</div>
-		 		<ul class="floor-tables-ul">
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">A01</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">A02</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">A03</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">A04</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">A05</p>
-		 			</li>
-		 		</ul>
-		 </div> 
-		  <div class="floor floor-2">
-			 	<div class="floor-text">Second Floor</div>
-		 		<ul class="floor-tables-ul">
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">B01</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">B02</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">B03</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 		</ul>
-		 </div> 
-		  <div class="floor floor-1">
-			 	<div class="floor-text">Third Floor</div>
-		 		<ul class="floor-tables-ul">
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">C01</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">C02</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">C03</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">C05</p>
-		 			</li>
-		 		</ul>
-		 </div> 
-		  <div class="floor floor-1">
-			 	<div class="floor-text">Four Floor</div>
-		 		<ul class="floor-tables-ul">
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">D01</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">D02</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">D03</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">D04</p>
-		 			</li>
-		 			<li class="floor-tables-ul-li">
-		 				<div class="floor-tables-ul-li-div"></div>
-		 				<p class="floor-tables-ul-li-p">D05</p>
-		 			</li>
-		 		</ul>
-		 </div> 
-	</div>
+		<div id="desk--bakcground"></div>
+		<div id="desk-inner">
+				<ul class="mui-table-view">
+				    <li class="mui-table-view-cell">
+				        <a class="mui-navigate-right">Number of Guests
+		        	    <span class="not-function mui-badge mui-badge-danger">Please Choose</span>
+				        </a>
+				    </li>
+				 </ul>  
+				 <div class="floor-ico">
+			 		<span><i class="iconfont icon-user iconfont-eated"></i><span class="floor-ico-text">Eated</span></span>
+			 		<span><i class="iconfont icon-lock iconfont-seize"></i><span class="floor-ico-text">Seize</span></span>
+			 		<span><i class="iconfont icon-yuding iconfont-destine"></i><span class="floor-ico-text">Destine</span></span>
+			 		<span><i class="iconfont icon-tuijian iconfont-recommended"></i><span class="floor-ico-text">Recommended</span></span>
+			 	</div>
+				 <div class="floor floor-1">
+					 	<div class="floor-text">First Floor</div>
+				 		<ul class="floor-tables-ul">
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">A01</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">A02</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">A03</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">A04</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">A05</p>
+				 			</li>
+				 		</ul>
+				 </div> 
+				  <div class="floor floor-2">
+					 	<div class="floor-text">Second Floor</div>
+				 		<ul class="floor-tables-ul">
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">B01</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">B02</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">B03</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 		</ul>
+				 </div> 
+				  <div class="floor floor-1">
+					 	<div class="floor-text">Third Floor</div>
+				 		<ul class="floor-tables-ul">
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">C01</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">C02</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">C03</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">C05</p>
+				 			</li>
+				 		</ul>
+				 </div> 
+				  <div class="floor floor-1">
+					 	<div class="floor-text">Four Floor</div>
+				 		<ul class="floor-tables-ul">
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">D01</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">D02</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">D03</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">D04</p>
+				 			</li>
+				 			<li class="floor-tables-ul-li">
+				 				<div class="floor-tables-ul-li-div"></div>
+				 				<p class="floor-tables-ul-li-p">D05</p>
+				 			</li>
+				 		</ul>
+				 </div> 
+			 </div>
+	 </div>
 </template>
 
 <script>
@@ -119,6 +122,33 @@ export default {
 </script>
 
 <style lang="css" scoped>
+.mui-navigate-right:after, .mui-push-left:after, .mui-push-right:after{
+	color:#fff;
+}
+#desk--bakcground{
+	height: 100vh;
+	width: 100%;
+	background:url(../assets/images/banner.jpg) no-repeat 50% /cover;
+	-webkit-filter: blur(50px);   
+    -moz-filter: blur(50px);   
+    -ms-filter: blur(50px);   
+    -o-filter: blur(50px);   
+    filter: blur(50px);   
+    filter: progid:DXImageTransform.Microsoft.Blur(PixelRadius=4, MakeShadow=false);  
+    position: absolute;
+    z-index: 1;
+    top: 0;
+}
+#desk-inner{
+	position: relative;
+	z-index: 3;
+	height: 100vh;
+	background: transparent;
+	color: #fff !important;
+}
+.mui-table-view{
+	background: transparent;
+}
 ul{
     overflow: hidden;
     margin: 0;
@@ -126,18 +156,15 @@ ul{
 }
 .not-function {
 	background: transparent;
-    color: #999;
     font-size: 16px;
 }
 .floor{
     font-size: 16px;
     overflow: hidden;
-    border-top: 1px solid #eee;
     margin-bottom: 10px;
 }
 .floor-text{	
 	text-indent: 15px;
-    color: #333;
     line-height: 45px;
 }
 .floor-ico{
@@ -145,15 +172,11 @@ ul{
     width: 100%;
     margin: auto;
     text-align: center;
-    background-color: #efeff4;
 }
 .floor-ico span{
 	padding:0 3px;	
-	color:#999;
 }
 .floor-ico .iconfont{
-    color: #f1f1f1;
-    border-radius: 100%;
     padding: 0.33em 0.37em;
     margin-right: 5px;
 }
@@ -171,12 +194,12 @@ ul{
 }
 .floor-tables-ul-li{
     float: left;
-    min-width: 85px;
+    min-width: 80px;
     margin: auto;
-    margin: 0px 7px;
+    margin: 0px 13px;
 }
 .floor-tables-ul-li-div{
-	background:#efeff4 url("../assets/images/logo.png") no-repeat center 9px /  50px 43px;
+	background:rgba(0,0,0,.2) url("../assets/images/logo.png") no-repeat center 9px /  50px 43px;
 	height: 58px;
     border-radius: 4px;
 }
@@ -184,5 +207,7 @@ ul{
 	text-align: center;
     margin-top: 5px;
     font-size: 15px;
+	color: #fff;
 }
+
 </style>
--- src/pages/menu.vue
+++ src/pages/menu.vue
@@ -1,6 +1,76 @@
 <template>
 	<div>
-		menu
+		<div class="banner">
+			<div class="banner-bottom">
+				<span class="bannert-text">weicanTImes experience systems</span>
+				<span class="banner-time">{{ DateStr }}</span>
+			</div>
+		</div>
+		<div class="menu">
+			<div class="mui-col-xs-3">
+				<div id="segmentedControls" class="mui-segmented-control mui-segmented-control-inverted mui-segmented-control-vertical">
+  
+  <a class="mui-control-item mui-active" data-index="0" href="#content1">选项1</a><a class="mui-control-item" data-index="1" href="#content2">选项2</a><a class="mui-control-item" data-index="2" href="#content3">选项3</a><a class="mui-control-item" data-index="3" href="#content4">选项4</a><a class="mui-control-item" data-index="4" href="#content5">选项5</a><a class="mui-control-item" data-index="5" href="#content6">选项6</a><a class="mui-control-item" data-index="6" href="#content7">选项7</a><a class="mui-control-item" data-index="7" href="#content8">选项8</a><a class="mui-control-item" data-index="8" href="#content9">选项9</a><a class="mui-control-item" data-index="9" href="#content10">选项10</a><a class="mui-control-item" data-index="10" href="#content11">选项11</a><a class="mui-control-item" data-index="11" href="#content12">选项12</a><a class="mui-control-item" data-index="12" href="#content13">选项13</a><a class="mui-control-item" data-index="13" href="#content14">选项14</a><a class="mui-control-item" data-index="14" href="#content15">选项15</a></div>
+			</div>
+			<div id="segmentedControlContents" class="mui-col-xs-9" style="border-left: 1px solid #c8c7cc;"><div id="content1" class="mui-control-content"><ul class="mui-table-view">
+
+			<li class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" src="../assets/images/logo.png">
+					<div class="mui-media-body">
+						<h4>Small steamed bun</h4>
+						<p class="mui-ellipsis"><span class="mui-ellipsis-left">Monthly sales 
+ 454</span><span class="mui-ellipsis-right">Applause Rate 100%</span></p>
+						<p class="money"><span class="money-left">$</span><span class="money-right">6</span></p>
+					</div>
+				</a>
+			</li>
+			<li class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" src="../assets/images/logo.png">
+					<div class="mui-media-body">
+						<h4>Small steamed bun</h4>
+						<p class="mui-ellipsis"><span class="mui-ellipsis-left">Monthly sales 
+ 454</span><span class="mui-ellipsis-right">Applause Rate 100%</span></p>
+						<p class="money"><span class="money-left">$</span><span class="money-right">6</span></p>
+					</div>
+				</a>
+			</li>
+			<li class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" src="../assets/images/logo.png">
+					<div class="mui-media-body">
+						<h4>Small steamed bun</h4>
+						<p class="mui-ellipsis"><span class="mui-ellipsis-left">Monthly sales 
+ 454</span><span class="mui-ellipsis-right">Applause Rate 100%</span></p>
+						<p class="money"><span class="money-left">$</span><span class="money-right">6</span></p>
+					</div>
+				</a>
+			</li>
+			<li class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" src="../assets/images/logo.png">
+					<div class="mui-media-body">
+						<h4>Small steamed bun</h4>
+						<p class="mui-ellipsis"><span class="mui-ellipsis-left">Monthly sales 
+ 454</span><span class="mui-ellipsis-right">Applause Rate 100%</span></p>
+						<p class="money"><span class="money-left">$</span><span class="money-right">6</span></p>
+					</div>
+				</a>
+			</li>
+			<li class="mui-table-view-cell mui-media">
+				<a href="javascript:;">
+					<img class="mui-media-object mui-pull-left" src="../assets/images/logo.png">
+					<div class="mui-media-body">
+						<h4>Small steamed bun</h4>
+						<p class="mui-ellipsis"><span class="mui-ellipsis-left">Monthly sales 
+ 454</span><span class="mui-ellipsis-right">Applause Rate 100%</span></p>
+						<p class="money"><span class="money-left">$</span><span class="money-right">6</span></p>
+					</div>
+				</a>
+			</li>
+			</ul></div></div>
+		</div>
 	</div>
 </template>
 
@@ -9,13 +79,130 @@ export default {
 
   name: 'menu',
 
+  
   data () {
     return {
-
+    	DateStr : "",
     };
+  },
+  methods : {
+	setDate () {
+		let self = this;
+		let date = new Date();
+		let year = date.getFullYear();
+		let money = money = date.getMonth() + 1;
+		let day = day = date.getDate();
+		let hour = date.getHours();
+        let minute = date.getMinutes();
+
+        self.DateStr = `${year}-${money}-${day} ${hour}:${minute}`
+	}
+  },
+  created () {
+	this.setDate();
   }
 };
 </script>
 
 <style lang="css" scoped>
+.menu{
+	clear:both;
+	overflow: hidden;
+	height: 480px;
+}
+.banner{
+	background:url("../assets/images/banner.jpg") no-repeat center center / cover;
+	width:100%;
+	height:30%;
+	min-height: 200px;	
+	position:relative;
+}
+.banner-bottom{
+	height:40px;
+	line-height: 40px;
+	color:#fff;
+	font-size:14px;
+	position:absolute;
+	bottom: 0;
+	background: rgba(0,0,0,.5);
+	width: 100%
+}
+.bannert-text{
+	float:left;
+	padding-left:15px;
+}
+.banner-time{
+	float:right;
+	padding-right:15px;
+
+}
+.mui-row.mui-fullscreen>[class*="mui-col-"] {
+	height: 100%;
+}
+.mui-row.mui-fullscreen>[class*="mui-col-"] {
+    height: 100%;
+}
+.mui-row>[class*=mui-col-] {
+    float: left;
+}
+.mui-col-xs-3,
+.mui-col-xs-9 {
+	overflow-y: auto;
+	height: 100%;
+	float:left;
+}
+
+.mui-segmented-control .mui-control-item {
+	line-height: 50px;
+	width: 100%;
+}
+
+.mui-control-content {
+	display: block;
+}
+
+.mui-segmented-control.mui-segmented-control-inverted .mui-control-item.mui-active {
+	background-color: #fff;
+}
+.money{
+	color:#f60;
+}
+.money-left{
+	margin-right: 3px;
+
+}
+.money-right{
+	font-weight: 700;
+	font-size:20px;
+	position: relative;
+	top: 1.4px;
+}
+
+.mui-ellipsis{
+    font-size: .293333rem;
+}
+
+.mui-ellipsis-left{
+	    float: left;
+	    margin-right: 10px
+}
+.mui-ellipsis-right{
+	    float: left;
+	
+}
+.mui-control-item{
+	position: relative;
+}
+.mui-control-item.mui-active::after{
+	content:" ";
+	position: absolute;
+	height: 100%;
+	width:4px;
+	left: 0;
+	top: 0;
+	background: burlywood
+}
+.mui-segmented-control.mui-segmented-control-inverted .mui-control-item.mui-active{
+	color:inherit;
+}
 </style>
